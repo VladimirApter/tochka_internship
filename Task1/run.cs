b5f4e8c395c4b380315221bcf081e44b4736658a
VladimirApter <vova.apter@gmail.com>
2025-05-04 12:02:43 +0500

    Task1 done

--- Task1/run.cs
+++ Task1/run.cs
@@ -1,3 +1,92 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
-Console.WriteLine("Hello, World!");
+
+class HotelCapacity
+{
+    static bool CheckCapacity(int maxCapacity, List<Guest> guests)
+    {
+        var occupancyDeltaByDate = new Dictionary<DateTime, int>();
+        foreach (var guest in guests)
+        {
+            var checkIn = DateTime.Parse(guest.CheckIn);
+            var checkOut = DateTime.Parse(guest.CheckOut);
+            
+            occupancyDeltaByDate.TryAdd(checkIn, 0);
+            occupancyDeltaByDate.TryAdd(checkOut, 0);
+            occupancyDeltaByDate[checkIn]++;
+            occupancyDeltaByDate[checkOut]--;
+        }
+
+        var capacity = 0;
+        foreach (var (date, delta) in occupancyDeltaByDate.OrderBy(x => x.Key))
+        {
+            capacity += delta;
+            if (capacity > maxCapacity)
+                return false;
+        }
+
+        return true;
+    }
+
+
+    class Guest
+    {
+        public string Name { get; set; }
+        public string CheckIn { get; set; }
+        public string CheckOut { get; set; }
+    }
+
+
+    static void Main()
+    {
+        var maxCapacity = int.Parse(Console.ReadLine());
+        var n = int.Parse(Console.ReadLine());
+
+
+        var guests = new List<Guest>();
+
+
+        for (var i = 0; i < n; i++)
+        {
+            var line = Console.ReadLine();
+            var guest = ParseGuest(line);
+            guests.Add(guest);
+        }
+
+
+        var result = CheckCapacity(maxCapacity, guests);
+
+
+        Console.WriteLine(result ? "True" : "False");
+    }
+
+
+    // Простой парсер JSON-строки для объекта Guest
+    static Guest ParseGuest(string json)
+    {
+        var guest = new Guest();
+
+
+        // Извлекаем имя
+        var nameMatch = Regex.Match(json, "\"name\"\\s*:\\s*\"([^\"]+)\"");
+        if (nameMatch.Success)
+            guest.Name = nameMatch.Groups[1].Value;
+
+
+        // Извлекаем дату заезда
+        var checkInMatch = Regex.Match(json, "\"check-in\"\\s*:\\s*\"([^\"]+)\"");
+        if (checkInMatch.Success)
+            guest.CheckIn = checkInMatch.Groups[1].Value;
+
+
+        // Извлекаем дату выезда
+        var checkOutMatch = Regex.Match(json, "\"check-out\"\\s*:\\s*\"([^\"]+)\"");
+        if (checkOutMatch.Success)
+            guest.CheckOut = checkOutMatch.Groups[1].Value;
+
+
+        return guest;
+    }
+}
