df08d7b452f9d0631935c6efbdb3a5abf675327d
VladimirApter <vova.apter@gmail.com>
2025-05-11 02:58:43 +0500

    Task1 to mono

--- Task1/run.cs
+++ Task1/run.cs
@@ -1,92 +1,84 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text.RegularExpressions;
+﻿using System.Text.RegularExpressions;
 
-
-class HotelCapacity
+namespace Task1
 {
-    static bool CheckCapacity(int maxCapacity, List<Guest> guests)
+    class HotelCapacity
     {
-        var occupancyDeltaByDate = new Dictionary<DateTime, int>();
-        foreach (var guest in guests)
+        static bool CheckCapacity(int maxCapacity, List<Guest> guests)
         {
-            var checkIn = DateTime.Parse(guest.CheckIn);
-            var checkOut = DateTime.Parse(guest.CheckOut);
-            
-            occupancyDeltaByDate.TryAdd(checkIn, 0);
-            occupancyDeltaByDate.TryAdd(checkOut, 0);
-            occupancyDeltaByDate[checkIn]++;
-            occupancyDeltaByDate[checkOut]--;
+            var occupancyDeltaByDate = new Dictionary<DateTime, int>();
+            foreach (var guest in guests)
+            {
+                var checkIn = DateTime.Parse(guest.CheckIn);
+                var checkOut = DateTime.Parse(guest.CheckOut);
+
+                occupancyDeltaByDate.TryAdd(checkIn, 0);
+                occupancyDeltaByDate.TryAdd(checkOut, 0);
+
+                occupancyDeltaByDate[checkIn]++;
+                occupancyDeltaByDate[checkOut]--;
+            }
+
+            var capacity = 0;
+            foreach (var kvp in occupancyDeltaByDate.OrderBy(x => x.Key))
+            {
+                capacity += kvp.Value;
+                if (capacity > maxCapacity)
+                    return false;
+            }
+
+            return true;
         }
 
-        var capacity = 0;
-        foreach (var (date, delta) in occupancyDeltaByDate.OrderBy(x => x.Key))
+        class Guest
         {
-            capacity += delta;
-            if (capacity > maxCapacity)
-                return false;
+            public string Name { get; set; }
+            public string CheckIn { get; set; }
+            public string CheckOut { get; set; }
         }
 
-        return true;
-    }
-
-
-    class Guest
-    {
-        public string Name { get; set; }
-        public string CheckIn { get; set; }
-        public string CheckOut { get; set; }
-    }
-
-
-    static void Main()
-    {
-        var maxCapacity = int.Parse(Console.ReadLine());
-        var n = int.Parse(Console.ReadLine());
-
-
-        var guests = new List<Guest>();
-
-
-        for (var i = 0; i < n; i++)
+        static void Main()
         {
-            var line = Console.ReadLine();
-            var guest = ParseGuest(line);
-            guests.Add(guest);
+            int maxCapacity;
+            int n;
+
+            if (!int.TryParse(Console.ReadLine(), out maxCapacity) || !int.TryParse(Console.ReadLine(), out n))
+            {
+                Console.WriteLine("Invalid input");
+                return;
+            }
+
+            var guests = new List<Guest>();
+
+            for (var i = 0; i < n; i++)
+            {
+                var line = Console.ReadLine();
+                var guest = ParseGuest(line);
+                guests.Add(guest);
+            }
+
+            var result = CheckCapacity(maxCapacity, guests);
+            Console.WriteLine(result ? "True" : "False");
         }
 
+        // Simple JSON parser for Guest object
+        static Guest ParseGuest(string json)
+        {
+            var guest = new Guest();
 
-        var result = CheckCapacity(maxCapacity, guests);
+            var nameMatch = Regex.Match(json, "\"name\"\\s*:\\s*\"([^\"]+)\"");
+            if (nameMatch.Success)
+                guest.Name = nameMatch.Groups[1].Value;
 
+            var checkInMatch = Regex.Match(json, "\"check-in\"\\s*:\\s*\"([^\"]+)\"");
+            if (checkInMatch.Success)
+                guest.CheckIn = checkInMatch.Groups[1].Value;
 
-        Console.WriteLine(result ? "True" : "False");
-    }
+            var checkOutMatch = Regex.Match(json, "\"check-out\"\\s*:\\s*\"([^\"]+)\"");
+            if (checkOutMatch.Success)
+                guest.CheckOut = checkOutMatch.Groups[1].Value;
 
-
-    // Простой парсер JSON-строки для объекта Guest
-    static Guest ParseGuest(string json)
-    {
-        var guest = new Guest();
-
-
-        // Извлекаем имя
-        var nameMatch = Regex.Match(json, "\"name\"\\s*:\\s*\"([^\"]+)\"");
-        if (nameMatch.Success)
-            guest.Name = nameMatch.Groups[1].Value;
-
-
-        // Извлекаем дату заезда
-        var checkInMatch = Regex.Match(json, "\"check-in\"\\s*:\\s*\"([^\"]+)\"");
-        if (checkInMatch.Success)
-            guest.CheckIn = checkInMatch.Groups[1].Value;
-
-
-        // Извлекаем дату выезда
-        var checkOutMatch = Regex.Match(json, "\"check-out\"\\s*:\\s*\"([^\"]+)\"");
-        if (checkOutMatch.Success)
-            guest.CheckOut = checkOutMatch.Groups[1].Value;
-
-
-        return guest;
+            return guest;
+        }
     }
 }
